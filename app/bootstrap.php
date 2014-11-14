702181f2a81331a07afb6b2af0d8a8b6dfa8b33f
Edwin van Maastrigt <evanmaastrigt@gmail.com>
2014-11-14 20:13:54 +0100

    Added ConfigServiceProvider

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,25 +1,26 @@
 <?php
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Knp\Provider\ConsoleServiceProvider;
+use Igorw\Silex\ConfigServiceProvider;
 use Silex\Application;
 use Silex\Provider\DoctrineServiceProvider;
 
 $app = new Application();
 $app['debug'] = true;
 
-$app->register(new DoctrineServiceProvider(), [
-    'db.options' => [
-        'driver' => 'pdo_mysql',
-        'host' => 'localhost',
-        'dbname' => 'silex',
-        'user' => 'root',
-        'password' => 'root',
-        'charset' => 'utf8',
-    ]
-]);
+$app->register(
+    new ConfigServiceProvider(realpath(__DIR__ . "/../app/config/config.php"))
+);
+
+$app->register(
+    new ConfigServiceProvider(realpath(__DIR__ . "/../app/config/service.php"))
+);
+
+$app->register(new DoctrineServiceProvider(), ['db.options' => $app['db.options']]);
+
 
 return $app;
---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'baseUrl' => 'http://silex.dev',
+    'db.options' => [
+        'driver' => 'pdo_mysql',
+        'host' => 'localhost',
+        'dbname' => 'silex',
+        'user' => 'root',
+        'password' => 'root',
+        'charset' => 'utf8',
+    ],
+];
