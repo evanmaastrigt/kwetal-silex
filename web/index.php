5b196aa6b8f83f21e6397d41117a2ef300736495
Edwin van Maastrigt <evanmaastrigt@gmail.com>
2014-11-16 19:09:36 +0100

    Added Twitter/Bootstrap

--- web/index.php
+++ web/index.php
@@ -5,14 +5,14 @@ ini_set('display_errors', 1);
 
 $app = require_once  __DIR__ . '/../app/bootstrap.php';
 
-//$app->get('/', function () {
-//    $output = 'Hello World!';
-//
-//    return $output;
-//});
+$app->get(
+    '/',
+    'Kwetal\Silex\Controller\Frontend\DefaultController::defaultAction'
+);
 
-$app->get('/', 'Kwetal\Silex\Controller\Frontend\DefaultController::defaultAction');
-
-$app->get('/admin', 'Kwetal\Silex\Controller\Backend\DefaultController::defaultAction');
+$app->get(
+    '/admin',
+    'Kwetal\Silex\Controller\Backend\DefaultController::defaultAction'
+);
 
 $app->run();
